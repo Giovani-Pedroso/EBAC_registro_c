b7557add6d515356a217f74e935e6aa9c997817f
Giovani <giovanisfpv@gmail.com>
2023-02-02 00:31:33 -0300

    thrid task sent

--- Hello_world.c
+++ Hello_world.c
@@ -7,11 +7,14 @@
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+// Function's declaration
 int registarNome();
 int consultarNome();
 int deletarNome();
 int errorMessage();
+void linuxPause();
 
 int main() {
   setlocale(LC_ALL, "Portuguese");
@@ -64,24 +67,95 @@ void linuxPause() {
 }
 
 int registarNome() {
-  /* printf("Digite um nome para registar: "); */
-  printf("Você escolheu registar um nome");
-  /* char nome[30]; */
-  /* scanf("%s", nome); */
+  // student information
+  // TODO convert to a struc
+  char cpf[40]; // It will be the primary key
+  char nome[40];
+  char sobrenome[40];
+  char cargo[40];
+
+  char arquivo[40];
+
+  printf("Digite o CPF: ");
+  scanf("%s", cpf);
   // To avoid the buffer overfollow
-  /* char c = getchar(); */
-  // Does not works on linux
-  /* system("pause"); */
+  char c = getchar();
+
+  strcpy(arquivo, cpf);
+
+  // Create a file with the same name that the cpf
+  // in the folder data you must create the folder
+  FILE *file;
+  // create the file ./data/[cpf].csv
+  char path[100];
+  // You need to create the folder "data" in your system
+  strcat(path, "./data/");
+  strcat(path, cpf);
+  strcat(path, ".csv");
+
+  file = fopen(path, "w");
+
+  // Print the cpf in the file
+
+  strcat(cpf, ",");
+  fprintf(file, cpf);
+
+  /* // pirnt the name in the file */
+  printf("Digite o nome: ");
+  scanf("%s", nome);
+  c = getchar();
+
+  fprintf(file, strcat(nome, ","));
+
+  /* // pirnt the sobrenome in the file */
+  printf("Digite o sobrenome: ");
+  scanf("%s", sobrenome);
+  c = getchar();
+  fprintf(file, strcat(sobrenome, ","));
+
+  /* // pirnt the sobrenome in the file */
+  printf("Digite o cargo: ");
+  scanf("%s", cargo);
+  c = getchar();
+  fprintf(file, strcat(cargo, ","));
+  fclose(file);
+
   linuxPause();
 
   return 0;
 }
 
 int consultarNome() {
-  printf("Você escolheu consultar os nomes");
+  setlocale(LC_ALL, "Portuguese");
 
-  // Does not works on linux
-  /* system("pause"); */
+  char cpf[40];
+  char conteudo[240];
+
+  printf("\n\nDigite o CPF a ser cosultado: ");
+  scanf("%s", cpf);
+  char c = getchar();
+
+  FILE *file;
+  char path[40];
+  strcat(path, "./data/");
+  strcat(path, cpf);
+  strcat(path, ".csv");
+
+  file = fopen(path, "r");
+
+  if (file == NULL) {
+    printf("CPF não encontrado");
+    linuxPause();
+    return 0;
+  }
+
+  while (fgets(conteudo, 240, file) != NULL) {
+    printf("\nEssas são as informações do usuario: ");
+    printf("%s\n\n", conteudo);
+    printf("");
+  }
+
+  fclose(file);
   linuxPause();
   return 0;
 }
